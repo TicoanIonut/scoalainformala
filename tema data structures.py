fe1d9b9fd1f6e63697efb33cc2f4b87b62d9034d
Ticoan Ionut <96721825+icefusionlol@users.noreply.github.com>
2022-01-20 22:46:49 +0200

    20.01.22

---
+++ tema data structures.py
@@ -0,0 +1,25 @@
+mylist=[7, 8, 9, 2, 3, 1, 4, 10, 5, 6]
+# mylist.sort()
+# print('lista ordonata',mylist)                    #lista ordonata ascendent
+
+# mylist.sort()
+# mylist.reverse()
+# print('lista ordonata',mylist)                    #lista ordonata inversata
+# print('lista ordonata',mylist[-1::-1])            #lista ordonata inversata(fara reverse)
+
+# mylist.sort()
+# print('numere pare',mylist[1::2])                 #numere pare
+
+# mylist.sort()
+# print('numere impare',mylist[0::2])               #numere impare
+
+# mylist.sort()
+# print(mylist)
+# del mylist[0::3]                                 # la  ora asta nu ma pot gandi la alta solutie cu slicing :(
+# del mylist[0::2]
+# print('multipli ai numarului 3',mylist)           #multipli ai numarului 3
+
+
+# mylist = [elem for elem in mylist if elem % 3== 0]
+# print(mylist)
+
