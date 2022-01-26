9652e1d8b177618f32cb141f4366fcb9a0b2954d
Ticoan Ionut <96721825+icefusionlol@users.noreply.github.com>
2022-01-26 21:29:31 +0200

    26.01.22

---
+++ curs03/programare_conditioala_si_looping.py
@@ -0,0 +1,34 @@
+# my_var=7
+# if my_var<6:
+#     print('set instru #1')
+# elif my_var<10:
+#     print('set instr #2')
+# else:
+#     print('set instr #3')
+# print('a iesit')
+# my_var=7
+# mesaj="set instr #3"
+# if my_var <6:
+#     mesaj= "set instr #1"
+# elif my_var<10:
+#     mesaj="set instr #2"
+# print(mesaj)
+# print("a iesit")
+# var=1
+# while var < 10:
+#     # var+=1
+#     print('bloc instr',var)
+#     var += 1
+#     # break
+# string ="ana are mere"
+# lista=[1,2,3,'a']
+# for i,v in enumerate(lista):
+#     print(i,'->',v)
+# for variabila_temporara in range(1,len(lista)+1):
+#     print(variabila_temporara)
+# print(variabila_temporara,'>>')
+dictionar= {1:'unu',2:'doi',3:'trei'}
+# for item,value in dictionar.items():
+#     print(item,value)
+for item in dictionar.keys():
+    print(item,dictionar.get(item))
---
+++ curs03/structuri_de_date.py
@@ -0,0 +1,23 @@
+lista=[1,0,2,1,'a']
+lista_1=['a','b','c']
+list_total=lista+lista_1
+# print(list_total)
+# tuplu=(1,2,3,1,'a')
+# tuplu_1=(1,2,3,'a')
+# tuplu_total=tuplu+tuplu_1
+# print(tuplu_total)
+# tuplu_nou=('1',)
+# print(type(tuplu_nou))
+# dictionar={'cheie':1,1:1,'lista':[1,2,3],'cheie':3}
+# print(dictionar['cheie'])
+# print(dictionar.get('cheie2'))
+# dictionar['cheie']=2
+# dictionar['cheie2']=3
+# dictionar.update({'cheie':2})
+# print(dictionar.keys())
+# print(dictionar.values())
+# print(dictionar.items())
+setul_meu={1,1,3}
+print(setul_meu)
+print(list_total)
+print(set(list_total))
