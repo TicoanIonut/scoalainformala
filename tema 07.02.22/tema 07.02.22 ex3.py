496d61e3728c69fd006d3625ba198de6841a936f
Ticoan Ionut <96721825+icefusionlol@users.noreply.github.com>
2022-01-27 10:44:49 +0200

    27.01.22

--- tema 07.02.22/tema 07.02.22 ex3.py
+++ tema 07.02.22/tema 07.02.22 ex3.py
@@ -1,12 +1,16 @@
-# def func(x):
-x=input()
-if x==int():
-    print(x)
-elif x==float():
-    print(x)
-elif x!=int():
+def func(score):
+    if zzz> 0:
+        print(sss)
+    elif score <= 0:
+        print(sss)
+    else:
+        print(0)
+    return score
+sss = input('>>')
+try:
+    zzz = float(sss)
+except:
     print(0)
-elif x!=float():
-    print(0)
-print(x)
-# (func(1))
+    quit()
+aaa = func(zzz)
+
