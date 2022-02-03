0fb7f355b11905ce771e032c63bc7356313925dc
Ticoan Ionut <96721825+icefusionlol@users.noreply.github.com>
2022-02-03 22:33:45 +0200

    02.02.22

--- tema 07.02.22/tema 07.02.22 ex3.py
+++ tema 07.02.22/tema 07.02.22 ex3.py
@@ -1,13 +1,10 @@
-def func(r):
+def func():
     in_put = input('>>')
     try:
         flt_in_put = float(in_put)
-        if flt_in_put>= 0:
-            print(in_put)
-        elif flt_in_put <= 0:
+        if flt_in_put>= 0 or flt_in_put <=0:
             print(in_put)
     except:
         print(0)
-
-func(0)
+func()
 
