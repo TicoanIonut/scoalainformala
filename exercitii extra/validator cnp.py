a72de9f59579b861317065ac22d1c42e4179ce56
Ticoan Ionut <96721825+icefusionlol@users.noreply.github.com>
2022-02-10 20:18:06 +0200

    10.02.22

--- exercitii extra/validator cnp.py
+++ exercitii extra/validator cnp.py
@@ -1,17 +1,29 @@
 import re
-import mmap
-import random
-def cnpval(s):
-    valid ="^[1-9]\d{2}(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])(0[1-9]|[1-4]\d|5[0-2]|99)(00[1-9]|0[1-9]\d|[1-9]\d\d)\d$"
-    if re.match(valid,s):
-        if s[0]==random.randint(0,9):
-            if datetime.s[1:6]:
-                print('sss')
+import datetime
+an='1234567890'
+cnp=input('enterCNP:')
+cnpc= int(2 * cnp[0] + 7 * cnp[1] + 2 * cnp[2] + 2 * cnp[3] + 2 * cnp[4] + 2 * cnp[5] +
+          2 * cnp[6] + 2 * cnp[7] + 2 * cnp[8] + 2 * cnp[9] + 2 * cnp[10] + 2 * cnp[11]+ 2 * cnp[12]) % 11
+date= cnp[1:7]
+# jud=['01','02','03','04','05','06','07','08','09','10','11','12','13'
+#     ,'14','15','16','17','18','19','20','21','22','23','24','25','26'
+#     ,'27','28','29','30','31','32','33','34','35','36','37','38','39'
+#     ,'40','41','42','43','44','45','46','47','48','49','51','51','52']
+#valid ="^[1-9]\d{2}(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])(0[1-9]|[1-4]\d|5[0-2]|99)(00[1-9]|0[1-9]\d|[1-9]\d\d)\d$"
+#folosind un regex(functionaza bine cu toate checurile, in afara de data)
+valid ="^[1-4]\d|5[0-2]\d$"
+def cnpval():
+    if datetime.datetime.strptime(date,"%y%m%d"):
+        if len(cnp) == 13:
+            if cnp[0] in an:
+                if re.match(valid,cnp[7:]):
+                    if cnp[12]==cnpc:     #aici crapa, trebuie testat
+                        return True
     else:
          return False
-
-cnp=input('enterCNP:')
-if cnpval(cnp)==True:
+if cnpval()==True:
      print(f'CNP Valid: {cnp}')
+else:
+    print('CNP invalid, mai incearca')
 
 
