d3315ed0fa49299a72259d7fa32bf169f98493b7
Ticoan Ionut <96721825+icefusionlol@users.noreply.github.com>
2022-02-18 20:47:41 +0200

    16.02.22

--- curs06/map_function.py
+++ curs06/map_function.py
@@ -7,29 +7,24 @@
 # print(list(rezultate))
 
 
-lista_numere = [1, 2, 3, 4]
-lista_numere2 = [5, 6, 7, 8, 9]
-# rezultate = map(lambda n, m: n + m, lista_numere,  lista_numere2)
-# print(list(rezultate))
+# lista_numere = [1, 2, 3, 4]
+# lista_numere2 = [5, 6, 7, 8, 9]
+# # rezultate = map(lambda n, m: n + m, lista_numere,  lista_numere2)
+# # print(list(rezultate))
+#
+#
+# def adunare(lista_numere, lista_numere2):
+#     suma = 0
+#     lista_adunare = []
+#     for i in range(0, min(len(lista_numere), len(lista_numere2))):
+#         lista_adunare.append((lista_numere[i] + lista_numere2[i]))
+#     # for i, v in enumerate(lista_numere):
+#     #     for j, w in enumerate(lista_numere2):
+#     #         # suama = lista_numere[i] + lista_numere2[j]
+#     #         suma = v + w
+#     #         if i == j:
+#     #             lista_adunare.append(suma)
+#     return lista_adunare
+# print(adunare(lista_numere, lista_numere2))
 
 
-def adunare(lista_numere, lista_numere2):
-    suma = 0
-    lista_adunare = []
-    for i in range(0, min(len(lista_numere), len(lista_numere2))):
-        lista_adunare.append((lista_numere[i] + lista_numere2[i]))
-    # for i, v in enumerate(lista_numere):
-    #     for j, w in enumerate(lista_numere2):
-    #         # suama = lista_numere[i] + lista_numere2[j]
-    #         suma = v + w
-    #         if i == j:
-    #             lista_adunare.append(suma)
-    return lista_adunare
-print(adunare(lista_numere, lista_numere2))
-
-
-#FILTER
-
-litera = ['a','b','c','d','e','i','j']
-
-ef filter_vocale(letter):
