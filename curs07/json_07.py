8d554288aafcc116893402bbd388016661ed973b
Ticoan Ionut <96721825+icefusionlol@users.noreply.github.com>
2022-02-23 21:40:44 +0200

    23.02.22

---
+++ curs07/json_07.py
@@ -0,0 +1,23 @@
+import json
+x = '{"name": "ion", "age": 30, "city": "iasi"}'
+y = json.loads(x)
+print(y, type(y))
+z = json.dumps(y)
+print(z, json.dumps(z), type(z))
+a = ["mere", "pere"]
+print(a, json.dumps(a), type(a))
+a = "hello"
+print(a, json.dumps(a), type(a))
+a = 42
+print(a, json.dumps(a), type(a))
+a = 31.75
+print(a, json.dumps(a), type(a))
+a = True
+print(a, json.dumps(a), type(a))
+a = None
+print(a, json.dumps(a), type(a))
+
+
+
+
+
