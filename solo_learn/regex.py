220b91c0bba8169ec84bb3d432b7189b81d5adba
Ticoan Ionut <96721825+icefusionlol@users.noreply.github.com>
2022-03-19 18:45:02 +0200

    19.03.2022

--- solo_learn/regex.py
+++ solo_learn/regex.py
@@ -11,8 +11,17 @@ import re
 
 # PHONE NUMBERE VALIDATOR ( must start withi 1 or 8 or 9, must be 8 digit long)
 
+# inp = input(">>")
+# path = r"^[9,8,1]{1,1}[0-9]{7}$"
+# z = re.match(path, inp)
+# if z:
+#     print('Valid')
+# else:
+#     print('Invalid')
+
+path = r'(0[1-9]|1[0-9]|2[0-9]|3[0-9]|4[06]|5[1-2])'
 inp = input(">>")
-path = r"^[9,8,1]{1,1}[0-9]{7}$"
+# path = r'(!47|48|49|50)[0-4][1-9]|51|52'
 z = re.match(path, inp)
 if z:
     print('Valid')
