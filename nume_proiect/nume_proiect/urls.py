75370e010a211304efdec780319b1bdd5f5b3dc3
Ticoan Ionut <96721825+icefusionlol@users.noreply.github.com>
2022-04-17 19:10:13 +0300

    17.04.2022

--- nume_proiect/nume_proiect/urls.py
+++ nume_proiect/nume_proiect/urls.py
@@ -1,7 +1,7 @@
 """nume_proiect URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import path
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('django.contrib.auth.urls'), {'next_page': '/'}, name='login'),
+    path('', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('locations/', include('aplicatie1.urls')),
 ]
