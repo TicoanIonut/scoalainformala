daf2c0763900572a2dc8c493ca1b2a68bd240426
Ticoan Ionut <96721825+icefusionlol@users.noreply.github.com>
2022-04-21 18:16:25 +0300

    21.04.2022

--- nume_proiect/aplicatie2/urls.py
+++ nume_proiect/aplicatie2/urls.py
@@ -5,8 +5,8 @@ from aplicatie2 import views
 app_name = 'companies'
 
 urlpatterns = [
-	path('adaugare/', views.CreateCompanyView.as_view(), name='add'),
 	path('', views.CompanyView.as_view(), name='listare'),
+	path('adaugare/', views.CreateCompanyView.as_view(), name='add'),
 	path('<int:pk>/update/', views.UpdateCompanyView.as_view(), name='modifica'),
 	path('<int:pk>/delete/', views.delete_company, name='sterge'),
 	path('<int:pk>/activeaza/', views.activate_company, name='activeaza'),
