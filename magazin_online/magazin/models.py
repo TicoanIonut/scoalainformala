26c8c7cebe0f7c112bcb5531767d34415c6fde69
Ticoan Ionut <96721825+icefusionlol@users.noreply.github.com>
2022-06-05 16:14:02 +0300

    05.06.2022

--- magazin_online/magazin/models.py
+++ magazin_online/magazin/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Client(models.Model):
@@ -11,6 +13,12 @@ class Client(models.Model):
 	def __str__(self):
 		return self.name
 	
+	
+@receiver(post_save, sender=User)
+def update_user_profile(sender, instance, created, **kwargs):
+	if created:
+		Client.objects.create(utilizator=instance)
+	instance.client.save()
 	
 class Produs(models.Model):
 	name = models.CharField(max_length=200, null=True)
